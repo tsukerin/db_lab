4eed256fbc68d4ffc43cfcd7dc860f17eb178af4
setusq <dustisla@yandex.ru>
2023-09-18 10:00:40 +0300

    Обновлен график "Возраст покупателей"

--- age_customers.py
+++ age_customers.py
@@ -1,12 +1,29 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-fig, ax = plt.subplots()
-plt.title('Возраст покупателей')
-df = pd.read_csv('Mall_Customers.csv', delimiter=',')
-plt.plot(df['Age'], df['CustomerID'])
-plt.xticks(df['Age'], df['CustomerID'])
+df = pd.read_csv('Mall_Customers.csv')
+
+plt.figure(figsize=(16, 8))
+
+step = 4
+
+plt.plot(df['CustomerID'][::step], df['Age'][::step], marker='o', linestyle='-', color='b', markersize=5, label='Возраст')
+plt.fill_between(df['CustomerID'][::step], df['Age'][::step], color='b', alpha=0.3)
+
+plt.title('Возраст покупателей', fontsize=16)
+plt.xlabel('ID Покупатель', fontsize=12)
+plt.ylabel('Возраст', fontsize=12)
+
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+
+plt.legend(loc='upper left', fontsize=12)
+
+for i, row in df[::step].iterrows():
+    plt.text(row['CustomerID'], row['Age'] + 2, f'{row["Age"]} лет', ha='center', fontsize=10)
+
+plt.savefig('lin_diagram.png')
 plt.show()
-fig.savefig('diagram.png')
+
